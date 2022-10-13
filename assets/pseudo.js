fd3cd8cb6909b8dfdf6f6cff4dfd3a1f36dc4658
Abigail Doyle <abigaildoyle3@gmail.com>
2022-10-12 17:39:37 -0700

    created pseudocode JS guesstimate

---
+++ assets/pseudo.js
@@ -0,0 +1,12 @@
+// link elements from html file
+// create questions
+// create multiple choice options
+// create answers
+// put questions, choices, and answers into an array
+// create a loop for the questions?
+// create timer?
+// end loop with timer?
+// count score?
+// form for initials?
+// saved form?
+// option to restart quiz?
