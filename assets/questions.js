49907759cb1fbe4665d0d90472c21d29f6b9f536
Abigail Doyle <abigaildoyle3@gmail.com>
2022-10-14 11:12:38 -0700

    added js file for questions

---
+++ assets/questions.js
@@ -0,0 +1,30 @@
+// decided to create a separate file for the questions so i could add as many as I wanted without clogging up the main file
+
+// choose var for overwrite
+
+var questions = {
+
+    {
+    question: "Of the following, if statements, which one correctly executes three instructions if the condition is true?";
+    mc: ["If (x < 0) a = b * 2; y = x; z = a – y;", "{ if (x < 0) a = b * 2; y = x; z = a – y; }", "If{ (x < 0) a = b * 2; y = x; z = a – y ; }", "If (x < 0) { a = b * 2; y = x; z = a – y; }"]
+    answer: "If (x < 0) { a = b * 2; y = x; z = a – y; }"
+    },
+
+    {
+    question:
+    mc:
+    answer:
+    }
+
+    {
+    question:
+    mc:
+    answer:
+    }
+
+    {
+    question:
+    mc:
+    answer:
+    }
+}
---
+++ assets/script.js
@@ -0,0 +1,7 @@
+var scores = document.querySelector("#scores");
+var timer = document.querySelector("#timer");
+var highscores = document.querySelector("#highscores");
+var questions = document.querySelector("#questions");
+var button = document.querySelector("#button");
+
+class 
