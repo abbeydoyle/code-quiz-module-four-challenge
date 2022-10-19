36e59fe804547c35167253f82d5849dabc0a0328
Abigail Doyle <abigaildoyle3@gmail.com>
2022-10-19 10:37:32 -0700

    work with tutor for initial question popup

--- assets/questions.js
+++ assets/questions.js
@@ -5,32 +5,32 @@
 var questions = [
 
     {
-    question: "Which type of JavaScript language is ___";
-    mc: ["Object-Oriented", "Object-Based", "Assembly-language", "High-level"]
+    question: "Which type of JavaScript language is ___",
+    mc: ["Object-Oriented", "Object-Based", "Assembly-language", "High-level"],
     answer: "Object-Based"
     },
 
     {
-    question: "Which one of the following also known as Conditional Expression:";
-    mc: ["Alternative to if-else", "Switch statement", "If-then-else statement", "immediate if"]
+    question: "Which one of the following also known as Conditional Expression:",
+    mc: ["Alternative to if-else", "Switch statement", "If-then-else statement", "immediate if"],
     answer: "immediate if"
     },
 
     {
-    question: "In JavaScript, what is a block of statement?";
-    mc: ["Conditional block", "block that combines a number of statements into a single compound statement", "both conditional block and a single statement", "block that contains a single statement"]
+    question: "In JavaScript, what is a block of statement?",
+    mc: ["Conditional block", "block that combines a number of statements into a single compound statement", "both conditional block and a single statement", "block that contains a single statement"],
     answer: "block that combines a number of statements into a single compound statement"
     },
 
     {
-    question: "When interpreter encounters an empty statements, what it will do:";
-    mc: ["Shows a warning", "Prompts to complete the statement", "Throws an error", "Ignores the statements"]
+    question: "When interpreter encounters an empty statements, what it will do:",
+    mc: ["Shows a warning", "Prompts to complete the statement", "Throws an error", "Ignores the statements"],
     answer: "Ignores the statements"
     },
     
     {
-    question: "The 'function' and 'var' are known as:";
-    mc: ["Keywords", "Data types", "Declaration statements", "Prototypes"]
+    question: "The 'function' and 'var' are known as:",
+    mc: ["Keywords", "Data types", "Declaration statements", "Prototypes"],
     answer: "Declaration statements"
     },
     
@@ -44,3 +44,4 @@ var questions = [
 
 
 ]
+
--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,40 @@
 var scores = document.querySelector("#scores");
 var timer = document.querySelector("#timer");
 var highscores = document.querySelector("#highscores");
-var questions = document.querySelector("#questions");
-var button = document.querySelector("#button");
+var questionsEL = document.querySelector("#questions");
+var startButton = document.querySelector("#button");
+var questionsContainer = document.querySelector(".questions-container");
+var mcEl = document.querySelector("#mc");
 
+
+
+function startQuiz() {
+     
+      cycleQuestions(0);
+
+}
+
+
+
+function cycleQuestions(index) {
+      let questionTag = document.createElement("p");
+      questionTag.textContent = questions[index].question;
+      questionsContainer.appendChild(questionTag);
+      let currentQuestion = questions[index];
+      for (let i = 0; i < currentQuestion.mc.length; i++) {
+            const choices = currentQuestion.mc[i];
+            console.log(choices, "sup");
+            const choiceButton = document.createElement("button");
+            choiceButton.textContent = choices;
+            mcEl.appendChild(choiceButton);
+      }
+}
+
+
+
+
+
+
+
+
+startButton.addEventListener("click", startQuiz);
