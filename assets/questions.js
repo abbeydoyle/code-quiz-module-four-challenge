d81709b01a4f494cbaae797765e65a751ea6698d
Abigail Doyle <abigaildoyle3@gmail.com>
2022-10-21 12:18:38 -0700

    moved highscore script and html to separate files

--- assets/questions.js
+++ assets/questions.js
@@ -3,44 +3,87 @@
 // choose var for overwrite
 
 var questions = [
-
     {
-    question: "Which type of language is Javascript?",
-    mc: ["Object-Oriented", "Object-Based", "Assembly-language", "High-level"],
-    answer: "Object-Based"
+      title: "Which one is a looping structure in JavaScript?",
+      choices: ["All the below", "For", "While", "do-while loops"],
+      answer: "All the below"
     },
-
     {
-    question: "Which one of the following also known as Conditional Expression:",
-    mc: ["Alternative to if-else", "Switch statement", "If-then-else statement", "immediate if"],
-    answer: "immediate if"
+      title: "What are the two basic groups of data types in JavaScript?",
+      choices: [
+        "Primitive and attribute",
+        "Primitive and reference types",
+        "Reference types and attribute",
+        "None of the above"
+      ],
+      answer: "Primitive and reference types"
     },
-
     {
-    question: "In JavaScript, what is a block of statement?",
-    mc: ["Conditional block", "block that combines a number of statements into a single compound statement", "both conditional block and a single statement", "block that contains a single statement"],
-    answer: "block that combines a number of statements into a single compound statement"
+      title: "Commonly used data types DO NOT include:",
+      choices: ["strings", "booleans", "alerts", "numbers"],
+      answer: "alerts"
     },
-
     {
-    question: "When interpreter encounters an empty statements, what it will do:",
-    mc: ["Shows a warning", "Prompts to complete the statement", "Throws an error", "Ignores the statements"],
-    answer: "Ignores the statements"
+      title: "Boolean operators that can be used in JavaScript include:",
+      choices: [
+        "'And' Operator &&",
+        "'Or' Operator ||",
+        "'Not' Operator !",
+        "All the above"
+      ],
+      answer: "All the above"
     },
-    
     {
-    question: "The 'function' and 'var' are known as:",
-    mc: ["Keywords", "Data types", "Declaration statements", "Prototypes"],
-    answer: "Declaration statements"
+      title:
+        "Which one of these is not among the three different types of errors in JavaScript?",
+      choices: [
+        "Animation time errors",
+        "Load time errors",
+        "Run time errors",
+        "Logical Errors"
+      ],
+      answer: "Animation time errors"
     },
-    
-    /*
     {
-    question: "";
-    mc: ["", "", "", ""]
-    answer: ""
+      title: "What is the data type of variables in JavaScript?",
+      choices: [
+        "Object data types",
+        "Function data type",
+        "None of the above",
+        "All of the above"
+      ],
+      answer: "Object data types"
     },
-    */
-
-
-]
+    {
+      title: "The condition in an if / else statement is enclosed within ____.",
+      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+      answer: "parentheses"
+    },
+    {
+      title: "Arrays in JavaScript can be used to store ____.",
+      choices: [
+        "numbers and strings",
+        "other arrays",
+        "booleans",
+        "all of the above"
+      ],
+      answer: "all of the above"
+    },
+    {
+      title:
+        "String values must be enclosed within ____ when being assigned to variables.",
+      choices: ["commas", "curly brackets", "quotes", "parentheses"],
+      answer: "quotes"
+    },
+    {
+      title:
+        "A very useful tool used during development and debugging for printing content to the debugger is:",
+      choices: ["JavaScript", "terminal / bash", "for loops", "console.log"],
+      answer: "console.log"
+    },
+    {
+      title: "What is the type of Pop up boxes available in JavaScript?:",
+      choices: ["Alert", "Confirm", "Prompt", "All the above"],
+      answer: "All the above"
+    }
+  ];
